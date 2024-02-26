89772b3bd2dd04cb483c6b7b5ea9270f154aaf8f
rab554628 <107383259+rab554628@users.noreply.github.com>
2024-02-26 09:28:24 +0530

    ww

--- src/App.tsx
+++ src/App.tsx
@@ -339,40 +339,8 @@ export default function Home() {
                   </span>
                 </p>
               )}
-              <h1 className="text-2xl font-bold line-clamp-1 xs:text-3xl lg:text-4xl">
-                {contractMetadata.isLoading ? (
-                  <div
-                    role="status"
-                    className="space-y-8 animate-pulse md:flex md:items-center md:space-x-8 md:space-y-0"
-                  >
-                    <div className="w-full">
-                      <div className="w-48 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></div>
-                    </div>
-                    <span className="sr-only">Loading...</span>
-                  </div>
-                ) : (
-                  contractMetadata.data?.name
-                )}
-              </h1>
-              {contractMetadata.data?.description ||
-                contractMetadata.isLoading ? (
-                  <div className="text-gray-500 line-clamp-2">
-                  {contractMetadata.isLoading ? (
-                    <div
-                      role="status"
-                        className="space-y-8 animate-pulse md:flex md:items-center md:space-x-8 md:space-y-0"
-                    >
-                      <div className="w-full">
-                        <div className="mb-2.5 h-2 max-w-[480px] rounded-full bg-gray-200 dark:bg-gray-700"></div>
-                        <div className="mb-2.5 h-2 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-                      </div>
-                      <span className="sr-only">Loading...</span>
-                    </div>
-                  ) : (
-                    contractMetadata.data?.description
-                  )}
-                </div>
-              ) : null}
+              
+              
             </div>
             <div className="flex w-full gap-4">
               {dropNotReady ? (
