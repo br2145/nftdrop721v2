ef8244df143d173d736a809cadcfa6a9e6c94e0e
rab554628 <107383259+rab554628@users.noreply.github.com>
2024-02-27 14:45:13 +0530

    ww

--- src/App.tsx
+++ src/App.tsx
@@ -305,7 +305,7 @@ export default function Home() {
       <ConnectWallet className="!absolute !right-4 !top-4" theme={theme} />
       <div className="grid h-screen grid-cols-1 lg:grid-cols-12">
         <div className="items-center justify-center hidden w-full h-full lg:col-span-5 lg:flex lg:px-12">
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
   <HeadingImage
     src={"https://assets-global.website-files.com/65d00699432af750589e6865/65d16de055751cc3144072f3_29.png"}
     isLoading={isLoading}
@@ -316,7 +316,7 @@ export default function Home() {
         <div className="flex items-center justify-center w-full h-full col-span-1 lg:col-span-7">
           <div className="flex flex-col w-full max-w-xl gap-4 p-12 rounded-xl lg:border lg:border-gray-400 lg:dark:border-gray-800">
             <div className="flex w-full mt-8 xs:mb-8 xs:mt-0 lg:hidden">
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
   <HeadingImage
     src={"https://assets-global.website-files.com/65d00699432af750589e6865/65d16de055751cc3144072f3_29.png"}
     isLoading={isLoading}
