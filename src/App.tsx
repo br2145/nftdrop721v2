a07ddcedbede54786e3b85476ba0b294206ded6c
rab554628 <107383259+rab554628@users.noreply.github.com>
2024-02-27 07:48:19 +0530

    ww

--- src/App.tsx
+++ src/App.tsx
@@ -305,18 +305,13 @@ export default function Home() {
       <ConnectWallet className="!absolute !right-4 !top-4" theme={theme} />
       <div className="grid h-screen grid-cols-1 lg:grid-cols-12">
         <div className="items-center justify-center hidden w-full h-full lg:col-span-5 lg:flex lg:px-12">
-        <HeadingImage
-             src={"/src/png1.png"}
-            isLoading={isLoading}
-          />
+        <img src="/src/png1.png" alt="NFT Collection" />
+
         </div>
         <div className="flex items-end justify-center w-full h-full col-span-1 lg:col-span-7">
           <div className="flex flex-col w-full max-w-xl gap-4 p-12 rounded-xl lg:border lg:border-gray-400 lg:dark:border-gray-800">
           <div className="flex w-full mt-8 xs:mb-8 xs:mt-0 lg:hidden">
-              <HeadingImage
-                 src={"/src/png1.png"}
-                isLoading={isLoading}
-              />
+          <img src="/src/png1.png" alt="NFT Collection" />
             </div>
             
             <div className="flex flex-col gap-2 xs:gap-4">
