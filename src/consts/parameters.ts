3191bd8aa44a412484cbff553a553de8238fed1f
rab554628 <107383259+rab554628@users.noreply.github.com>
2024-05-22 08:23:49 +0530

    ww

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,7 +2,7 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0xf5aACbC75070268Bf2c02c894Ab18536DcB7e38f";
+export const contractConst = "0x4398e0cB7E818c0C666DBc636707CF74B9e72A2C";
 
 // The name of the chain your contract is deployed to.
 // Refer to README.md on how to specify the chain name.
@@ -12,7 +12,7 @@ export const chainConst = 1;
 // Please ensure that you define the correct domain for your API key from the API settings page.
 // You can get one for free at https://thirdweb.com/create-api-key
 // Learn more here: https://blog.thirdweb.com/changelog/api-keys-to-access-thirdweb-infra
-export const clientIdConst = "318c0de1c5b95cbd464f3785d9ab97a4";
+export const clientIdConst = "06255a1fe24f71acb294431c04a8fe76";
 
 // Configure the primary color for buttons and other UI elements
 export const primaryColorConst = "red"; 
